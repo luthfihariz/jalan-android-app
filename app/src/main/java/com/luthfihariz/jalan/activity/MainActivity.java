055ccfed9477d62e542d09ac7f8c92be291d9fb4
Luthfi Hariz <luthfi.hariz@tiket.com>
2016-10-31 20:33:54 +0700

    Initial commit.

---
+++ app/src/main/java/com/luthfihariz/jalan/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.luthfihariz.jalan.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.luthfihariz.jalan.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
